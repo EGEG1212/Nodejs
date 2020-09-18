c783bbc6d35be824218dc875732927f444d6f6af
Eungyeong <dana1anne@naver.com>
2020-09-18 14:48:27 +0900

    first commit

---
+++ helloworld.js
@@ -0,0 +1,16 @@
+console.log('Hello World');
+
+let sum=0;
+for (let i=1; i<=100; i++)
+    sum += i;
+
+console.log(`1에서 100까지 정수의 합은 ${sum}입니다.`);
+// 백코트로 엄청 간편해졌다
+console.log('1에서 100까지 정수의 합은 ' + sum + '입니다.');
+// 원래 자바
+
+let msg = `1에서 100까지 정수의 합은
+${sum}
+입니다. `;
+console.log(msg);
+// 줄바꿈반영
