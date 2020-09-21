32e850179265d7f31e7695f8497776405d01a73a
Eungyeong <dana1anne@naver.com>
2020-09-21 18:12:02 +0900

    반복문

---
+++ 04.반복문/06.백준while더하기사이클.js
@@ -0,0 +1,32 @@
+/* for (let i=2; i<1000; i++) {
+    약수구하기
+    for(let k=1; k<i; k++) {
+        약수배열
+    }
+    약수배열의 합
+}
+ */
+
+for (let i=2; i<1000; i++) {
+    int n, cnt, i;
+    int main()
+    {
+        scanf("%d, &n");
+
+        i=1;
+        while(i<=n)
+        {
+            if(n%i==0)
+            {
+                cnt+=1;
+                printf("%d ", i);
+            }
+            i++
+        }
+        print(cnt);
+    }
+    for(let k=1; k<i; k++) {
+        약수배열
+    }
+    약수배열의 합
+}
