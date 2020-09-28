6bf91a1eeaea84f069ab3a85521e3c00faf447dc
Eungyeong <dana1anne@naver.com>
2020-09-28 16:27:29 +0900

    http 모듈

--- 10.http/12.POSTparameta받기.js
+++ 10.http/12.POSTparameta받기.js
@@ -1,14 +1,15 @@
 const http = require('http');
+const url = require('url')
 const qs = require('querystring');          // querystring신기술ㅋㅋ
 const fs = require('fs');
 
 http.createServer(function(req, res) {
-    let method = req.method;               
-    if (method === 'GET') { // 이것이 첫 화면. 제출버튼 누르니
+    let pathname = url.parse(req.url).pathname;
+    if (pathname === '/') { // 이것이 첫 화면. 제출버튼 누르니
         fs.readFile('view/12.form.html', 'utf8', (error, html) => {
             res.end(html)
         });
-    } else if (method === 'POST') {
+    } else if (pathname === '/proc') {
         let body = '';
         req.on('data', function(data) { //긴~데이터가 들어오면 바디에 붙이고붙이고
             body += data;
